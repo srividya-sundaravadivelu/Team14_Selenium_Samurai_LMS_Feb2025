87d681683c22bd80f9f428b947caded19e4485a5
srividya-sundaravadivelu <srividya-sundaravadivelu>
2025-02-20 14:44:03 -0600

    reverted changes to Hooks and TestContext

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,19 +1,21 @@
 package hooks;
 
 import java.io.ByteArrayInputStream;
-
-import io.cucumber.java.*;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import driver.TestContext;
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.qameta.allure.Allure;
 import utils.ConfigReader;
 import utils.LogHelper;
 import utils.WebDriverWaitUtility;
 
 public class Hooks {
-	private static TestContext testContext;
+	private TestContext testContext;
 
 	public Hooks(TestContext testContext) {
 		this.testContext = testContext;
@@ -47,10 +49,10 @@ public class Hooks {
 		}
 	}
 
-//	@After
-//	public void tearDown() {
-//		LogHelper.info("Tearing down WebDriver...");
-//		testContext.quitDriver();
-//		LogHelper.info("Tear down WebDriver successful");
-//	}
+	@After
+	public void tearDown() {
+		LogHelper.info("Tearing down WebDriver...");
+		testContext.quitDriver();
+		LogHelper.info("Tear down WebDriver successful");
+	}
 }
